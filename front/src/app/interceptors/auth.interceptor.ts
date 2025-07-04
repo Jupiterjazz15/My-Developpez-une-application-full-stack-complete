b6b07756ad30f3b9331a338e6bf9f5b298146e93
coraliehaller <haller.coralie.15@gmail.com>
2025-07-04 11:27:34 +0200

    intercepteur : modification pour qu'il n'envoie pas de token pour register

--- front/src/app/interceptors/auth.interceptor.ts
+++ front/src/app/interceptors/auth.interceptor.ts
@@ -1,4 +1,3 @@
-// src/app/services/auth.interceptor.ts
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -10,8 +9,8 @@ export class AuthInterceptor implements HttpInterceptor {
     // Récupérer le token depuis le localStorage
     const token = localStorage.getItem('token'); // Assure-toi que le token est bien stocké dans localStorage
 
-    // Si un token existe, on l'ajoute dans l'en-tête Authorization
-    if (token) {
+    // Si un token existe et que la route n'est pas /register, on l'ajoute dans l'en-tête Authorization
+    if (token && !req.url.includes('/register')) {
       const cloned = req.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`  // Ajouter le token dans l'en-tête
@@ -21,7 +20,7 @@ export class AuthInterceptor implements HttpInterceptor {
       return next.handle(cloned); // Continuer avec la requête modifiée
     }
 
-    // Si aucun token, on continue sans ajouter l'en-tête Authorization
+    // Si aucun token ou si la route est "/register", on continue sans ajouter l'en-tête Authorization
     return next.handle(req);
   }
 }
