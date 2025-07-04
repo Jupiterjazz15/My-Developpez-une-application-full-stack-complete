e6ace44b2aa43c3a483c9a28302b70611d449518
coraliehaller <haller.coralie.15@gmail.com>
2025-07-04 10:49:32 +0200

    CORS

--- back/src/main/java/com/openclassrooms/mddapi/configuration/WebConfig.java
+++ back/src/main/java/com/openclassrooms/mddapi/configuration/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // Permet toutes les routes et autorise les requêtes de localhost:4200
         registry.addMapping("/**") // Autoriser toutes les routes de l'API
-                .allowedOrigins("http://localhost:4200")  // Permet les requêtes depuis le frontend Angular
+                .allowedOrigins("*")  // Permet les requêtes depuis le frontend Angular
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Les méthodes HTTP autorisées
                 .allowedHeaders("*");  // Permet tous les en-têtes
     }
